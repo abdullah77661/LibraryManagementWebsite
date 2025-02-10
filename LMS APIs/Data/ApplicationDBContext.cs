deedf36f10d7d01625e2a3a4f968182454504c3c
DESKTOP-P380498\Abdullah <abdullahmazhar389@gmail.com>
2025-02-10 15:45:40 +0500

    Book Category interface class added

--- LMS APIs/Data/ApplicationDBContext.cs
+++ LMS APIs/Data/ApplicationDBContext.cs
@@ -13,5 +13,7 @@ namespace LMS_APIs.Data
         }
         public DbSet<Author> Authors { get; set; }
         public DbSet<Book> Books  { get; set; }
+
+        public DbSet<BookCategory> Category { get; set; }
     }
 }
---
+++ LMS APIs/Repository/ICategory.cs
@@ -0,0 +1,13 @@
+﻿using LMS_APIs.Models;
+
+namespace LMS_APIs.Repository
+{
+    public interface ICategory
+    {
+        public IEnumerable<BookCategory> GetCategory();
+        BookCategory GetCategoryById(int id);
+        public BookCategory PostCategory(BookCategory category);
+        BookCategory UpdateCategory(int id, BookCategory category);
+        bool DeleteCategory(int id);
+    }
+}
