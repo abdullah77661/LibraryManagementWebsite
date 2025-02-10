6bd92aa9b11706279a5488fc9957ac0b8cf05dcd
DESKTOP-P380498\Abdullah <abdullahmazhar389@gmail.com>
2025-02-10 16:09:04 +0500

    User controller added

---
+++ LMS APIs/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMS_APIs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            return Ok("Get all users");
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetUserById(int id)
+        {
+            return Ok($"Get user with id {id}");
+        }
+        [HttpPost]
+        public IActionResult PostUser()
+        {
+            return Ok("Create a new user");
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdateUser(int id)
+        {
+            return Ok($"Update user with id {id}");
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteUser(int id)
+        {
+            return Ok($"Delete user with id {id}");
+        }
+    }
+}
